d9a516cb9cc3461b87c18c14dd07516360d230a2
jabdulrahim <61425219+jabdulrahim@users.noreply.github.com>
2020-05-26 17:12:50 -0400

    Add files via upload

---
+++ PythonAPI/Twitter-API-Instructions/config.py
@@ -0,0 +1,4 @@
+consumer_key = "mPjpd58IX5r3n8Q49634SW64S"
+consumer_secret = "KIEfe1cvWlOm8bQEqrETkpHcaDDUsXEOV8hZEwdAice4Jva7vy"
+access_token = "2267060929-Qgru73sHiRkIMEPjDmAwqcytXHkZiSIaVvq8wY8"
+access_token_secret = "fUH9sjGw3FYYkTmY8HJA9yG0YCug9XhvxrynAydktxhuf" 
---
+++ PythonAPI/Weather-API-Instructions/starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "b81553c506805b592dc17281ce0f3663"
+
+# Google API Key
+g_key = "AIzaSyCmKn2A2bhzDZM0GI6vb0ge-SUYRTg0uOU"

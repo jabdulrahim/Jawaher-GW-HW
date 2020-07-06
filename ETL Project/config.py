f4e744d1248dfb0775e9edc84d74fa4cc6b85b16
jabdulrahim <61425219+jabdulrahim@users.noreply.github.com>
2020-07-05 20:19:35 -0400

    Add files via upload

---
+++ ETL Project/config.py
@@ -0,0 +1,13 @@
+#SalesForce api
+sf_username = 'jawaher@dartanion.net'
+sf_password = 'Dartlings1!'
+sf_security_token = 'hos604FGlSmKpGet9Yz6gJX8b'
+
+
+
+#MySql 
+remote_db_endpoint = 'database-1.crsck7wmt2gh.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = 'Jawaher_20'
+remote_db_name = 'gwsis'

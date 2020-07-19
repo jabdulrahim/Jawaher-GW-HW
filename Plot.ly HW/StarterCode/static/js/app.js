d3b2fd4860d915aada1bb8d3bb520b990c41fb82
jabdulrahim <61425219+jabdulrahim@users.noreply.github.com>
2020-07-19 16:08:17 -0400

    Add files via upload

--- Plot.ly HW/StarterCode/static/js/app.js
+++ Plot.ly HW/StarterCode/static/js/app.js
@@ -3,7 +3,7 @@ function getPlot(id) {
     d3.json("Data/samples.json").then((data)=> {
         console.log(data)
   
-        var wfreq = data.metadata.map(d => d.wfreq)
+        var wfreq = data.metadata.filter(s => s.id.toString() === id).map(d => d.wfreq)
         console.log(`Washing Freq: ${wfreq}`)
         
         // filter sample values by id 
@@ -89,30 +89,116 @@ function getPlot(id) {
   
         // The guage chart
   
+        // //var data_g = [
+        //   {
+        //   domain: { x: [0, 1], y: [0, 1] },
+        //   value: parseFloat(wfreq),
+        //   title: { text: `Weekly Washing Frequency ` },
+        //   type: "indicator",
+          
+        //   mode: "gauge+number",
+        //   gauge: { axis: { range: [null, 9] },
+        //            steps: [
+        //             { range: [0, 2], color: "yellow" },
+        //             { range: [2, 4], color: "cyan" },
+        //             { range: [4, 6], color: "teal" },
+        //             { range: [6, 8], color: "lime" },
+        //             { range: [8, 9], color: "green" },
+        //           ]}
+              
+        //   }
+        // ];
+
+        // var layout_g = { 
+        //     width: 700, 
+        //     height: 600, 
+        //     margin: { t: 20, b: 40, l:100, r:100 } 
+        //   };
+        var level = parseFloat(wfreq) * 20;
+        // Trig to calc meter point
+        var degrees = 180 - level;
+        var radius = 0.5;
+        var radians = (degrees * Math.PI) / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
+        // Path: may have to change to create a better triangle
+        var mainPath = "M -.0 -0.05 L .0 0.05 L ";
+        var pathX = String(x);
+        var space = " ";
+        var pathY = String(y);
+        var pathEnd = " Z";
+        var path = mainPath.concat(pathX, space, pathY, pathEnd);
         var data_g = [
           {
-          domain: { x: [0, 1], y: [0, 1] },
-          value: parseFloat(wfreq),
-          title: { text: `Weekly Washing Frequency ` },
-          type: "indicator",
-          
-          mode: "gauge+number",
-          gauge: { axis: { range: [null, 9] },
-                   steps: [
-                    { range: [0, 2], color: "yellow" },
-                    { range: [2, 4], color: "cyan" },
-                    { range: [4, 6], color: "teal" },
-                    { range: [6, 8], color: "lime" },
-                    { range: [8, 9], color: "green" },
-                  ]}
-              
+            type: "scatter",
+            x: [0],
+            y: [0],
+            marker: { size: 12, color: "850000" },
+            showlegend: false,
+            name: "Freq",
+            text: level,
+            hoverinfo: "text+name"
+          },
+          {
+            values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+            rotation: 90,
+            text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+            textinfo: "text",
+            textposition: "inside",
+            marker: {
+              colors: [
+                "rgba(0, 105, 11, .5)",
+                "rgba(10, 120, 22, .5)",
+                "rgba(14, 127, 0, .5)",
+                "rgba(110, 154, 22, .5)",
+                "rgba(170, 202, 42, .5)",
+                "rgba(202, 209, 95, .5)",
+                "rgba(210, 206, 145, .5)",
+                "rgba(232, 226, 202, .5)",
+                "rgba(240, 230, 215, .5)",
+                "rgba(255, 255, 255, 0)"
+              ]
+            },
+            labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+            hoverinfo: "label",
+            hole: 0.5,
+            type: "pie",
+            showlegend: false
           }
         ];
-        var layout_g = { 
-            width: 700, 
-            height: 600, 
-            margin: { t: 20, b: 40, l:100, r:100 } 
-          };
+        var layout_g = {
+          shapes: [
+            {
+              type: "path",
+              path: path,
+              fillcolor: "850000",
+              line: {
+                color: "850000"
+              }
+            }
+          ],
+          title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+          height: 500,
+          width: 500,
+          xaxis: {
+            zeroline: false,
+            showticklabels: false,
+            showgrid: false,
+            range: [-1, 1]
+          },
+          yaxis: {
+            zeroline: false,
+            showticklabels: false,
+            showgrid: false,
+            range: [-1, 1]
+          }
+        };
+
+
+
+
+
+
         Plotly.newPlot("gauge", data_g, layout_g);
       });
   }  
